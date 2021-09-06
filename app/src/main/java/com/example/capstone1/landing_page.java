c953dc1e65d2e4dc89257c1276a342c5db7ab11f
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-09-06 23:47:59 +0800

    layouts and constraints

--- app/src/main/java/com/example/capstone1/landing_page.java
+++ app/src/main/java/com/example/capstone1/landing_page.java
@@ -4,19 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class landing_page extends AppCompatActivity {
 
+
+    Animation topAnim;
+    ImageView image;
     private ImageView button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_landing_page);
 
-        ImageView button = (ImageView) findViewById(R.id.nextBtn);
+        //Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+
+        image = findViewById(R.id.logo);
+
+        image.setAnimation(topAnim);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
