d1da51f8216e1984ad428b2ef9a7b4d30bbbbf60
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-09-20 10:50:16 +0800

    layout for edit/delete schedule(cardview design)

---
+++ app/src/main/java/com/example/capstone1/edit_delete_schedule_activity.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_delete_schedule_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_delete_schedule);
+    }
+}
