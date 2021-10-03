68c27f48e044fbf197a2aebe22cb9e28a80255e7
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-10-03 15:27:28 +0800

    card view for today measurement,logo icon

---
+++ app/src/main/java/com/example/capstone1/cardview_today_measurement.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cardview_today_measurement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardview_today_measurement);
+    }
+}
