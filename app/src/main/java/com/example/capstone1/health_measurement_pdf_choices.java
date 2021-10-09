37510e26d581c01188636bdb56c12890450ea8d8
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-10-09 20:00:13 +0800

    layouts pdf

---
+++ app/src/main/java/com/example/capstone1/health_measurement_pdf_choices.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class health_measurement_pdf_choices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_measurement_pdf_choices);
+    }
+}
---
+++ app/src/main/java/com/example/capstone1/pdf_hours_of_sleep.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class pdf_hours_of_sleep extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf_hours_of_sleep);
+    }
+}
