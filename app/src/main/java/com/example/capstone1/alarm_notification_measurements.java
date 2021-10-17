b88587f07c8c5164fd18ce5a7df2eb5afdb3c754
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-10-17 08:32:53 +0800

    health measurement alarm layout

---
+++ app/src/main/java/com/example/capstone1/alarm_notification_measurements.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alarm_notification_measurements extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_notification_measurements);
+    }
+}
