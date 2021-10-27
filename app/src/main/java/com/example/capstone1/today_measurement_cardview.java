6e31f84256834436a21a0e4ad7b07d80ee2cbd64
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-10-27 23:12:52 +0800

    today_measurement_cardview, added divider for his

---
+++ app/src/main/java/com/example/capstone1/today_measurement_cardview.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class today_measurement_cardview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_measurement_cardview);
+    }
+}
