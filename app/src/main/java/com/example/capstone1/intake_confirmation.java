73d3908a66cd2d064965c4cde36010c8ac8b5e85
lquinto13 <78460138+lquinto13@users.noreply.github.com>
2021-10-29 05:48:28 +0800

    fixed skipping in intake confirmation

--- app/src/main/java/com/example/capstone1/intake_confirmation.java
+++ app/src/main/java/com/example/capstone1/intake_confirmation.java
@@ -218,6 +218,7 @@ public class intake_confirmation extends AppCompatActivity {
 
 
                 }
+                startActivity(new Intent(intake_confirmation.this, today_page_recycler.class));
 
             }
         });
