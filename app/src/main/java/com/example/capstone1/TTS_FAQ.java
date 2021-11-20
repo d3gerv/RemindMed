4eded5bc2e5f65a0e1f0bdc61bbf70451c290818
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-11-20 17:04:33 +0800

    FAQ

---
+++ app/src/main/java/com/example/capstone1/TTS_FAQ.java
@@ -0,0 +1,14 @@
+package com.example.capstone1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TTS_FAQ extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tts_faq);
+    }
+}
