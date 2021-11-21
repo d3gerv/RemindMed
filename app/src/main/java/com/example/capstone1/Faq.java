07ba3bfded9321b18d2cae6cd5f9812b690b92d5
arahnicolle <arahnicolle.tolentino.iics@ust.edu.ph>
2021-11-21 15:46:26 +0800

    FAQ

--- app/src/main/java/com/example/capstone1/Faq.java
+++ app/src/main/java/com/example/capstone1/Faq.java
@@ -1,14 +1,65 @@
 package com.example.capstone1;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Faq extends AppCompatActivity {
+
+   // public CardView card1, card2, card3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_faq);
+
+     /*   card1 = (CardView) findViewById(R.id.FAQ_OCR);
+        card2 = (CardView) findViewById(R.id.FAQ_TTS);
+        card3 = (CardView) findViewById(R.id.FAQ_type);
+
+        card1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Faq.this, OCR_FAQ.class);
+                startActivity(intent);
+            }
+        });
+
+        card2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Faq.this, TTS_FAQ.class);
+                startActivity(intent);
+            }
+        });
+
+        card3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Faq.this, TypeUnit_FAQ.class);
+                startActivity(intent);
+            }
+        });*/
+
+    }
+
+    public void Faq_To_User(View view) {
+        Intent intent = new Intent(Faq.this, user_information.class);
+        startActivity(intent);
+    }
+
+    public void FAQOCR_Faq(View view) {
+        Intent intent = new Intent(Faq.this, OCR_FAQ.class);
+        startActivity(intent);
+    }
+    public void FAQTTS_Faq(View view) {
+        Intent intent = new Intent(Faq.this, FAQTTS.class);
+        startActivity(intent);
+    }
+    public void FAQUnit_Faq(View view) {
+        Intent intent = new Intent(Faq.this, TYPE_FAQ.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/capstone1/OCR_FAQ.java
+++ app/src/main/java/com/example/capstone1/OCR_FAQ.java
@@ -1,8 +1,10 @@
 package com.example.capstone1;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class OCR_FAQ extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class OCR_FAQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocr_faq);
+
+    }
+
+    public void FacOcr_To_Faq(View view) {
+        Intent intent = new Intent(OCR_FAQ.this, Faq.class);
+        startActivity(intent);
     }
 }
