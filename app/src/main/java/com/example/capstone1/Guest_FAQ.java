a477b6f5cb0c20029978fb47b1938b712bad048e
gmusita30 <60341467+gmusita30@users.noreply.github.com>
2021-11-22 19:01:15 +0800

    faq

--- app/src/main/java/com/example/capstone1/Guest_FAQ.java
+++ app/src/main/java/com/example/capstone1/Guest_FAQ.java
@@ -1,6 +1,8 @@
 package com.example.capstone1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,9 @@ public class Guest_FAQ extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_faq);
+    }
+    public void GuestFAQ_Faq(View view) {
+        Intent intent = new Intent(Guest_FAQ.this, Faq.class);
+        startActivity(intent);
     }
 }
